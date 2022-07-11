975276159bb2d65f05b6bfc98532c7674476c3c8
Carson <carsongro@gmail.com>
2022-07-10 19:05:52 -0600

    added timer and colors

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Carson Gross on 7/9/22.
+//
+
+import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+    
+    static let example = Card(prompt: "Who played the 13th doctor in Doctor Who?", answer: "Jodie Wittaker")
+}
